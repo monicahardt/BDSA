1fb6172bf11d6b1c9395cc5c730a7ec6f492c64b
monicahardt <monha@itu.dk>
2022-09-06 13:03:51 +0200

    Created user interface for IsLeapYear

--- MyApp/MyApp.Tests/LeapYearTests.cs
+++ MyApp/MyApp.Tests/LeapYearTests.cs
@@ -6,10 +6,9 @@ public class LeapYearTests
     public void calculate_if_leap_year_true()
     {
            // Arrange
-        var year = new Year();
 
         //Act
-        var IsLeapYear = year.isLeapYear(2016);
+        var IsLeapYear = Year.isLeapYear(2016);
 
         // Assert
         IsLeapYear.Should().Be(true);
@@ -19,10 +18,9 @@ public class LeapYearTests
     public void calculate_if_leap_year_false()
     {
            // Arrange
-        var year = new Year();
 
         //Act
-        var IsLeapYear = year.isLeapYear(7);
+        var IsLeapYear = Year.isLeapYear(7);
 
         // Assert
         IsLeapYear.Should().Be(false);
@@ -32,12 +30,42 @@ public class LeapYearTests
     public void calculate_if_leap_year_divisible_by_400_true()
     {
            // Arrange
-        var year = new Year();
-
         //Act
-        var IsLeapYear = year.isLeapYear(800);
+        var IsLeapYear = Year.isLeapYear(800);
 
         // Assert
         IsLeapYear.Should().Be(true);
     }
+
+    [Fact]
+    public void check_if_output_is_yay_leap_year_true()
+    {
+        // Given
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        
+        // When
+        Year.isInputLeapYear("800");
+        
+        // Then
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("yay");
+    }
+
+    [Fact]
+    public void check_if_output_is_nay_leap_year_false()
+    {
+        // Given
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        
+        // When
+        Year.isInputLeapYear("1000");
+        
+        // Then
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("nay");
+    }
 }
+
+
--- MyApp/MyApp/Year.cs
+++ MyApp/MyApp/Year.cs
@@ -2,16 +2,32 @@ namespace MyApp;
 
 public class Year
 {
-    public bool isLeapYear(int year)
+
+    public static void Main (string[]args){
+        Console.WriteLine("Type a year");
+        var inputYear = Console.ReadLine();
+        isInputLeapYear(inputYear);
+    }
+    
+    public static bool isLeapYear(int year)
     {
         if(year%4 == 0){
-            if(year%100 == 0 && year%400 == 0){
-                    return true;
+            if(year%100 == 0 && year%400 != 0){
+                    return false;
                 } else {
                 return true; 
             }
         } else {
             return false;
+        }
+    }
+
+    public static void isInputLeapYear(string s)  {
+        int inputYearInt = Convert.ToInt32(s);
+        if(isLeapYear(inputYearInt)){
+            Console.WriteLine("yay");
+        } else {
+            Console.WriteLine("nay");
         }
     }
 }
